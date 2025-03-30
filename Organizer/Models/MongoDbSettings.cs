b26752fa1a39b5d4741f6476ec4de62daa18c35f
slayyyyyyy <andreea.gurzu02@gmail.com>
2025-03-30 20:34:27 +0300

    Add project files.

---
+++ Organizer/Models/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Organizer.Models
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string UsersCollectionName { get; set; } = string.Empty;
+    }
+
+}
---
+++ Organizer/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Organizer.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("email")]
+    public string Email { get; set; } = string.Empty;
+}
+
